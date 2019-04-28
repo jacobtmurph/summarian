26a21db7141d8b3a58f3f55c5ed699de7328cc28
Jacob T. Murphy <jacobtrevmurphy@gmail.com>
2019-04-28 14:38:34 +0100

    Add base files, dependencies, and a basic startup for the Express server

---
+++ src/index.js
@@ -0,0 +1,47 @@
+// Load modules
+const express = require('express');
+const jsonParser = require('body-parser').json;
+const mongoose = require('mongoose');
+
+// Set up the app
+const app = express();
+
+// Set up a MongoDB connection
+mongoose.connect("mongodb://localhost:27017/summarian", {useNewUrlParser: true});
+const db = mongoose.connection;
+
+// If database connection is successfull
+db.on('connected', console.log.bind(console, `Successfully connected to the ${db.name} database.`));
+
+// In the case of a Mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+
+
+
+// Set the port
+app.set('port', process.env.PORT || 3000);
+
+// Parse incoming json requests
+app.use(jsonParser());
+
+// Send 404 if no other route matched
+app.use((req, res) => {
+    res.status(404).json({
+      message: 'Route Not Found'
+    })
+  })
+  
+  // Global error handler
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).json({
+      message: err.message,
+      error: {}
+    });
+  });
+  
+  // start listening on our port
+  const server = app.listen(app.get('port'), () => {
+    console.log(`Express server is listening on port ${server.address().port}`);
+  });
+  
